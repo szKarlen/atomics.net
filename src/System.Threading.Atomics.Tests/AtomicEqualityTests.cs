9475b0c1c7abce9772a06b258dc6faa57170f25c
Karlen Simonyan <szkarlen@gmail.com>
2015-08-20 18:04:10 +0300

    Unit-tests converage of Atomic<T> numerics support

--- src/System.Threading.Atomics.Tests/AtomicEqualityTests.cs
+++ src/System.Threading.Atomics.Tests/AtomicEqualityTests.cs
@@ -126,5 +126,82 @@ namespace System.Threading.Atomics.Tests
 
             Assert.True(atomicBoolean != SessionValue);
         }
+
+        /************************/
+
+        public static IEnumerable<object[]> Primitive_Value_Change_Data
+        {
+            get
+            {
+                yield return new object[] {byte.MinValue, byte.MaxValue, (byte) 0, (byte) 123};
+                yield return new object[] { sbyte.MinValue, sbyte.MaxValue, (sbyte)0, (sbyte)123 };
+                yield return new object[] { short.MinValue, short.MaxValue, (short)0, (short)123 };
+                yield return new object[] { ushort.MinValue, ushort.MaxValue, (ushort)0, (ushort)123 };
+                yield return new object[] { double.MinValue, double.MaxValue, 0d, 123d };
+                yield return new object[] { float.MinValue, float.MaxValue, 0f, 123f };
+                yield return new object[] { ulong.MinValue, ulong.MaxValue, 0ul, 123ul };
+            }
+        }
+
+        [Theory]
+        [MemberData("Primitive_Value_Change_Data")]
+        public void Atomic_Primitive_Value_Change_Should_Success<T>(T minValue, T maxValue, T defaultValue, T comparand) where T : struct, IEquatable<T>
+        {
+            var atomicInteger = new Atomic<T>(maxValue);
+            Assert.Equal(maxValue, atomicInteger.Value);
+
+            atomicInteger.Value = minValue;
+            Assert.Equal(minValue, atomicInteger.Value);
+
+            atomicInteger.Value = defaultValue;
+            Assert.Equal(defaultValue, atomicInteger.Value);
+
+            // same value assignment
+            atomicInteger.Value = defaultValue;
+            Assert.Equal(defaultValue, atomicInteger.Value);
+
+            atomicInteger.Value = comparand;
+            Assert.Equal(comparand, atomicInteger.Value);
+            atomicInteger.Value = comparand;
+            Assert.Equal(comparand, atomicInteger.Value);
+        }
+
+        public static IEnumerable<object[]> Primitive_IEquatable_Of_Primitive_Data
+        {
+            get
+            {
+                yield return new object[] { byte.MinValue, byte.MaxValue };
+                yield return new object[] { sbyte.MinValue, sbyte.MaxValue };
+                yield return new object[] { short.MinValue, short.MaxValue };
+                yield return new object[] { ushort.MinValue, ushort.MaxValue };
+                yield return new object[] { double.MinValue, double.MaxValue };
+                yield return new object[] { float.MinValue, float.MaxValue };
+                yield return new object[] { ulong.MinValue, ulong.MaxValue };
+            }
+        }
+
+        [Theory]
+        [MemberData("Primitive_IEquatable_Of_Primitive_Data")]
+        public void Atomic_Primitive_IEquatable_Of_Primitive_Should_Compare<T>(T minValue, T maxValue) where T : struct, IEquatable<T>
+        {
+            IEquatable<T> firstAtomic = new Atomic<T>(maxValue);
+            IEquatable<T> secondAtomic = new Atomic<T>(minValue);
+
+            Assert.False(firstAtomic.Equals(minValue));
+            Assert.False(secondAtomic.Equals(maxValue));
+        }
+
+        [Theory]
+        [MemberData("Primitive_IEquatable_Of_Primitive_Data")]
+        public void Atomic_Primitive_Implicit_Reassignment_Should_Change_Reference<T>(T minValue, T maxValue) where T : struct, IEquatable<T>
+        {
+            var firstAtomic = new Atomic<T>(maxValue);
+            var secondAtomic = firstAtomic;
+
+            firstAtomic = minValue;
+
+            Assert.NotEqual(secondAtomic, firstAtomic);
+            Assert.False(object.ReferenceEquals(secondAtomic, firstAtomic));
+        }
     }
 }
