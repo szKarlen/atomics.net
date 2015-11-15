924f38a07eca1843e767bccdfbf28c61b80c51b3
Karlen Simonyan <szkarlen@gmail.com>
2015-11-15 17:50:28 +0300

    .NET4 support

---
+++ src/System.Threading.Atomics/Misc.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System.Threading.Atomics
+{
+#if NET40
+    class Volatile
+    {
+        public static T Read<T>(ref T value)
+        {
+            return Platform.Read(ref value);
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Constructor | AttributeTargets.Method, Inherited = false)]
+    public sealed class MethodImplAttribute : Attribute
+    {
+        public MethodImplOptions Value { get; private set; }
+
+        public MethodImplAttribute(MethodImplOptions methodImplOptions)
+        {
+            this.Value = methodImplOptions;
+        }
+    }
+
+    [Flags]
+    public enum MethodImplOptions
+    {
+        NoInlining = 8,
+        NoOptimization = 64,
+        PreserveSig = 128,
+        AggressiveInlining = 256
+    }
+
+    
+#endif
+}
